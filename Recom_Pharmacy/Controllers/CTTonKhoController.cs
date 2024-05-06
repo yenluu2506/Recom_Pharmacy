134fed4ce8ba04a2deeeb294fc410a920676ce75
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2024-05-06 21:52:09 +0700

    update chi tiet ton kho

---
+++ Recom_Pharmacy/Controllers/CTTonKhoController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Web.UI;
+using Recom_Pharmacy.Models;
+
+namespace Recom_Pharmacy.Controllers
+{
+    public class CTTonKhoController : Controller
+    {
+        private RecomPharmacyEntities db = new RecomPharmacyEntities();
+
+        // GET: CTTonKho
+        public ActionResult Index(string Searchtext, int? page, int? SelectedTonKho, int? SelectedThuoc)
+        {
+            ViewBag.TonKho = new SelectList(db.TONKHOes.ToList(), "ID");
+            ViewBag.Thuoc = new SelectList(db.TONKHOes.ToList(), "ID", "TENTHUOC");
+            var pageSize = 5;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<CTTONKHO> items = db.CTTONKHOes.OrderByDescending(x => x.ID);
+            if (SelectedTonKho.HasValue)
+            {
+                items = items.Where(x => x.TONKHO.ID == SelectedTonKho.Value);
+            }
+            if (SelectedThuoc.HasValue)
+            {
+                items = items.Where(x => x.THUOC.ID == SelectedThuoc.Value);
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.SelectedTonKho = SelectedTonKho;
+            ViewBag.SelectedThuoc = SelectedThuoc;
+            ViewBag.page = page;
+            return View(items);
+        }
+
+        // GET: CTTonKho/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTTONKHO cTTONKHO = db.CTTONKHOes.Find(id);
+            if (cTTONKHO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTTONKHO);
+        }
+
+        // GET: CTTonKho/Create
+        public ActionResult Add()
+        {
+            ViewBag.MATONKHO = new SelectList(db.TONKHOes, "ID", "ID");
+            ViewBag.MATHUOC = new SelectList(db.THUOCs, "ID", "TENTHUOC");
+            return View();
+        }
+
+        // POST: CTTonKho/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add([Bind(Include = "ID,MATHUOC,MATONKHO,SLTON,TRANGTHAI")] CTTONKHO cTTONKHO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CTTONKHOes.Add(cTTONKHO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MATONKHO = new SelectList(db.TONKHOes, "ID", "ID", cTTONKHO.MATONKHO);
+            ViewBag.MATHUOC = new SelectList(db.THUOCs, "ID", "TENTHUOC", cTTONKHO.MATHUOC);
+            return View(cTTONKHO);
+        }
+
+        // GET: CTTonKho/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTTONKHO cTTONKHO = db.CTTONKHOes.Find(id);
+            if (cTTONKHO == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MATONKHO = new SelectList(db.TONKHOes, "ID", "ID", cTTONKHO.MATONKHO);
+            ViewBag.MATHUOC = new SelectList(db.THUOCs, "ID", "TENTHUOC", cTTONKHO.MATHUOC);
+            return View(cTTONKHO);
+        }
+
+        // POST: CTTonKho/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MATHUOC,MATONKHO,SLTON,TRANGTHAI")] CTTONKHO cTTONKHO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cTTONKHO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MATONKHO = new SelectList(db.TONKHOes, "ID", "ID", cTTONKHO.MATONKHO);
+            ViewBag.MATHUOC = new SelectList(db.THUOCs, "ID", "TENTHUOC", cTTONKHO.MATHUOC);
+            return View(cTTONKHO);
+        }
+
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = db.CTTONKHOes.Find(id);
+            if (item != null)
+            {
+                item.TRANGTHAI = !item.TRANGTHAI;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isAcive = item.TRANGTHAI });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.CTTONKHOes.Find(id);
+            if (item != null)
+            {
+                db.CTTONKHOes.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.CTTONKHOes.Find(Convert.ToInt32(item));
+                        db.CTTONKHOes.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ Recom_Pharmacy/Controllers/TonKhoController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Web.UI;
+using Recom_Pharmacy.Models;
+using Filter = Recom_Pharmacy.Models.Common.Filter;
+
+namespace Recom_Pharmacy.Controllers
+{
+    public class TonKhoController : Controller
+    {
+        private RecomPharmacyEntities db = new RecomPharmacyEntities();
+
+        // GET: TonKho
+        public ActionResult Index(string Searchtext, int? page, int? SelectedKho)
+        {
+            ViewBag.Kho = new SelectList(db.KHOes.ToList(), "ID", "TENKHO");
+            var pageSize = 5;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<TONKHO> items = db.TONKHOes.OrderByDescending(x => x.ID);
+            if (SelectedKho.HasValue)
+            {
+                items = items.Where(x => x.KHO.ID == SelectedKho.Value);
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.SelectedKho = SelectedKho;
+            ViewBag.page = page;
+            return View(items);
+        }
+
+        // GET: TonKho/Create
+        public ActionResult Add()
+        {
+            ViewBag.MAKHO = new SelectList(db.KHOes, "ID", "TENKHO");
+            return View();
+        }
+
+        // POST: TonKho/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add([Bind(Include = "ID,MAKHO,NGAY,TRANGTHAI")] TONKHO tONKHO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TONKHOes.Add(tONKHO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MAKHO = new SelectList(db.KHOes, "ID", "TENKHO", tONKHO.MAKHO);
+            return View(tONKHO);
+        }
+
+        // GET: TonKho/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TONKHO tONKHO = db.TONKHOes.Find(id);
+            if (tONKHO == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MAKHO = new SelectList(db.KHOes, "ID", "TENKHO", tONKHO.MAKHO);
+            return View(tONKHO);
+        }
+
+        // POST: TonKho/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MAKHO,NGAY,TRANGTHAI")] TONKHO tONKHO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tONKHO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MAKHO = new SelectList(db.KHOes, "ID", "TENKHO", tONKHO.MAKHO);
+            return View(tONKHO);
+        }
+
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = db.TONKHOes.Find(id);
+            if (item != null)
+            {
+                item.TRANGTHAI = !item.TRANGTHAI;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isAcive = item.TRANGTHAI });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.TONKHOes.Find(id);
+            if (item != null)
+            {
+                db.TONKHOes.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.TONKHOes.Find(Convert.ToInt32(item));
+                        db.TONKHOes.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
