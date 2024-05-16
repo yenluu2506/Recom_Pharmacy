6bd8bd4fed8732f04bbffac30209d798b2e71cc4
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2024-05-16 21:42:30 +0700

    update view all product

--- Recom_Pharmacy/Controllers/UserInterfaceController.cs
+++ Recom_Pharmacy/Controllers/UserInterfaceController.cs
@@ -38,5 +38,37 @@ namespace Recom_Pharmacy.Controllers
             var t = from a in db.KHACHHANGs where a.Username == ac.Username select a;
             return View(t.ToList());
         }
+
+        public ActionResult ViewAllProduct(string search)
+        {
+            var model = db.THUOCs.Where(x => x.TRANGTHAI == true).OrderByDescending(c => c.NGAYSX).Where(nv => nv.TENTHUOC.Contains(search) || search == null && nv.TRANGTHAI == true).ToList();
+
+            return View(model);
+        }
+        private List<THUOC> NewItem(int count)
+        {
+            return db.THUOCs.Where(d => d.TRANGTHAI == true).OrderByDescending(a => a.NGAYSX).Take(count).ToList();
+        }
+        public ActionResult NewProduct()
+        {
+            return PartialView(NewItem(5));
+        }
+        public ActionResult Menu()
+        {
+            var menu = from t in db.MENUTHUOCs select t;
+            return PartialView(menu);
+        }
+        public ActionResult LoaiThuoc(int id)
+        {
+            var b = (from t in db.LOAITHUOCs where t.MAMENU == id select t).ToList();
+
+            return PartialView(b);
+        }
+
+        public ActionResult ProductByLT(int id)
+        {
+            var pr = from d in db.THUOCs where d.MALOAI == id && d.TRANGTHAI == true select d;
+            return View(pr);
+        }
     }
 }
