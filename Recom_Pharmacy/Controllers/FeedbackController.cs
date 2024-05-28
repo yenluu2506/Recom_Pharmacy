29ee84d5e13ab85e400e7be8c724a5f1872688cb
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2024-05-28 21:42:32 +0700

    update feedback

---
+++ Recom_Pharmacy/Controllers/FeedbackController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Web.UI;
+using Recom_Pharmacy.Models;
+using Filter = Recom_Pharmacy.Models.Common.Filter;
+
+namespace Recom_Pharmacy.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private RecomPharmacyEntities db = new RecomPharmacyEntities();
+
+        // GET: Feedback
+        public ActionResult Index(string Searchtext, int? page)
+        {
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<Feedback> items = db.Feedbacks.OrderByDescending(x => x.ID);
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                string searchKeyword = Filter.ChuyenCoDauThanhKhongDau(Searchtext);
+                items = items.Where(x => Filter.ChuyenCoDauThanhKhongDau(x.HOTEN).StartsWith(searchKeyword, StringComparison.OrdinalIgnoreCase) ||
+                                         Filter.ChuyenCoDauThanhKhongDau(x.HOTEN).Contains(searchKeyword) ||
+                                         x.HOTEN.Contains(Searchtext));
+
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.page = page;
+            return View(items);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Feedbacks.Find(id);
+            if (item != null)
+            {
+                db.Feedbacks.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.Feedbacks.Find(Convert.ToInt32(item));
+                        db.Feedbacks.Remove(obj);
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
